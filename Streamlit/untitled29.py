dc3c90ac3493dc48c0d3cdf18a215051bc8b691a
Allan <47067864+ohallao@users.noreply.github.com>
2025-01-11 23:32:27 -0300

    Update untitled29.py

--- Streamlit/untitled29.py
+++ Streamlit/untitled29.py
@@ -5,24 +5,117 @@ import matplotlib.pyplot as plt
 
 # Configuração das colunas e pesos por posição
 colunas_por_posicao = {
-    "Goleiros": [...],
-    "Meias Ofensivos": [...],
-    "Pontas": [...],
-    "Volantes": [...],
-    "Zagueiros": [...],
-    "Lat": [...],
-    "Atacantes": [...]
-}
+    "Goleiros": [
+            "jogador", "time", "posicao", "Minutos", "GA", "SoTA", "Saves", "Saves%", "Clean Sheet", 
+            "Clean Sheet%", "PSxG", "PSxG+/-", "Passes Att", "Passes Launch%", "Passes AvgLen", 
+            "Crosses Opp", "Crosses Stp", "Crosses Stp%", "Sweeper #OPA", "Sweeper #OPA/90", "Sweeper AvgDist."
+        ],
+        "Meias Ofensivos": [
+            "jogador", "time", "posicao", "Minutos", "xG", "xAG", "G-PK", "Assistencias", "PrgC", "PrgP", 
+            "Key_Pass", "Passing 1/3", "Cross", "Dribles_Tentados", "Dribles_certos"
+        ],
+        "Pontas": [
+            "jogador", "time", "posicao", "idade", "Minutos", "G-PK", "Assist", "xG", "xAG", "xG+xAg", 
+            "Prgc", "PrgP", "Cross", "Faltas Sofridas", "KeyPass", "1/3", "passPA", "crossPA", 
+            "Dribles tentados", "Dribles Certos", "Corridas 1/3", "Corridas PA", "PrgR"
+        ],
+        "Volantes": [
+            "jogador", "time", "posicao", "idade", "Minutos", "Prgc", "PrgP", "Amarelo", "Vermelho", 
+            "2 Amarelo", "Falta Cometida", "interceptacao", "recuperacoes", "Duelos Aereoes W", 
+            "Duelos Aereoes L", "Duelos Aereoes W%", "Divididas Total", "Divididas Ganhas", "Clearence", "Erros"
+        ],
+        "Zagueiros": [
+            "jogador", "time", "posicao", "Minutos", "Prgc", "PrgP", "Aerial Duel%", "Aerial Duel Won", 
+            "Aerial Duel Lost", "Fouls", "Yellow Card", "Red Card", "2 Yellow Card", "Desarmes Certos", 
+            "Desarmes Totais", "Desarmes%", "Interceptacao", "Erros"
+        ],
+        "Laterais": [
+            "jogador", "time", "posicao", "idade", "Minutos", "Prgc", "PrgP", "xAG", "Aerial Duel%", 
+            "Aerial Duel Won", "Aerial Duel Lost", "Fouls", "Yellow Card", "Red Card", "2 Yellow Card", "Cross", 
+            "Desarmes Certos", "Desarmes Totais", "Desarmes%", "Interceptacao", "Erros"
+        ],
+        "Atacantes": [
+            "jogador", "time", "posicao", "Minutos", "PrgR", "xG", "xAG", "G-PK", "Assistencias", 
+            "Acoes Ofensivas", "Aerial Duel%"
+        ]
+    }
 
 pesos_por_posicao = {
-    "Goleiros": {...},
-    "Meias Ofensivos": {...},
-    "Pontas": {...},
-    "Volantes": {...},
-    "Zagueiros": {...},
-    "Lat": {...},
-    "Atacantes": {...}
-}
+    "Goleiros": {
+            'GA': -5,
+            'Saves': 2,
+            'Clean Sheet': 10,
+            'PSxG': 5,
+            'Passes Launch%': 3,
+            'Crosses Stp%': 5,
+            'Sweeper #OPA': 5
+        },
+        "Meias Ofensivos": {
+            'xG': 1.1,
+            'xAG': 1.85,
+            'G-PK': 1.80,
+            'Assistencias': 2,
+            'Acoes Ofensivas': 1.2,
+            'Key_Pass': 1.5,
+            'Passing 1/3': 1.1,
+            'Cross': 1.0,
+            '%Dribles': 1.25
+        },
+        "Pontas": {
+            'xG+xAg': 10,
+            'KeyPass': 5,
+            'Cross': 8,
+            '1/3': 2,
+            'Dribles Certos': 5,
+            'Corridas 1/3': 2,
+            'Corridas PA': 7,
+            'passPA': 8,
+            'crossPA': 8
+        },
+        "Volantes": {
+            'Amarelo': -5,
+            'Vermelho': -10,
+            'Falta Cometida': -2,
+            'interceptacao': 2,
+            'recuperacoes': 2,
+            'Duelos Aereoes W': 4,
+            'Divididas Ganhas': 6,
+            'Clearence': 5,
+            'Erros': 1
+        },
+        "Zagueiros": {
+            'Prgc': 2,
+            'PrgP': 2,
+            'Aerial Duel Won': 10,
+            'Fouls': -3,
+            'Yellow Card': -5,
+            'Red Card': -10,
+            'Desarmes Certos': 10,
+            'Interceptacao': 5,
+            'Erros': -2
+        },
+        "Laterais": {
+            'Prgc': 2,
+            'PrgP': 2,
+            'xAG': 10,
+            'Fouls': -3,
+            'Yellow Card': -5,
+            'Red Card': -10,
+            'Cross': 10,
+            'Desarmes Certos': 10,
+            'Interceptacao': 5,
+            'Erros': -2
+        },
+        "Atacantes": {
+            'PrgR': 4,
+            'xG': 5,
+            'xAG': 4,
+            'G-PK': 10.0,
+            'Assistencias': 3,
+            'Acoes Ofensivas': 2,
+            'Aerial Duel%': 2
+        }
+    }
 
 # Função para normalizar uma métrica
 def normalizar(coluna):
